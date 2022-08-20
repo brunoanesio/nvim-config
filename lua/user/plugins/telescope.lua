1a3a7ee7528eb5fc1ec4bedd448fe04acf687316
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2022-08-20 02:52:30 -0300

    Update telescope config

--- lua/user/plugins/telescope.lua
+++ lua/user/plugins/telescope.lua
@@ -1,8 +1,73 @@
+local actions = require("telescope.actions")
 require("telescope").setup({
 	defaults = {
 		prompt_prefix = " ",
 		selection_caret = " ",
 		path_display = { "smart" },
+	},
+	mappings = {
+		i = {
+			["<C-n>"] = actions.cycle_history_next,
+			["<C-p>"] = actions.cycle_history_prev,
+
+			["<C-j>"] = actions.move_selection_next,
+			["<C-k>"] = actions.move_selection_previous,
+
+			["<C-b>"] = actions.results_scrolling_up,
+			["<C-f>"] = actions.results_scrolling_down,
+
+			["<C-c>"] = actions.close,
+
+			["<Down>"] = actions.move_selection_next,
+			["<Up>"] = actions.move_selection_previous,
+
+			["<CR>"] = actions.select_default,
+			["<C-s>"] = actions.select_horizontal,
+			["<C-v>"] = actions.select_vertical,
+			["<C-t>"] = actions.select_tab,
+
+			["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+			["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+			["<C-l>"] = actions.complete_tag,
+			["<C-h>"] = actions.which_key,
+			["<esc>"] = actions.close,
+		},
+		n = {
+			["<esc>"] = actions.close,
+			["<CR>"] = actions.select_default,
+			["<C-x>"] = actions.select_horizontal,
+			["<C-v>"] = actions.select_vertical,
+			["<C-t>"] = actions.select_tab,
+			["<C-b>"] = actions.results_scrolling_up,
+			["<C-f>"] = actions.results_scrolling_down,
+
+			["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+			["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+
+			["j"] = actions.move_selection_next,
+			["k"] = actions.move_selection_previous,
+			["H"] = actions.move_to_top,
+			["M"] = actions.move_to_middle,
+			["L"] = actions.move_to_bottom,
+			["q"] = actions.close,
+			["dd"] = require("telescope.actions").delete_buffer,
+			["s"] = actions.select_horizontal,
+			["v"] = actions.select_vertical,
+			["t"] = actions.select_tab,
+
+			["<Down>"] = actions.move_selection_next,
+			["<Up>"] = actions.move_selection_previous,
+			["gg"] = actions.move_to_top,
+			["G"] = actions.move_to_bottom,
+
+			["<C-u>"] = actions.preview_scrolling_up,
+			["<C-d>"] = actions.preview_scrolling_down,
+
+			["<PageUp>"] = actions.results_scrolling_up,
+			["<PageDown>"] = actions.results_scrolling_down,
+
+			["?"] = actions.which_key,
+		},
 	},
 	extensions = {
 		fzf = {
