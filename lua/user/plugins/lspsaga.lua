ea0db9fcc14c34065410e3fdb07ce525ac1c9a28
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2022-09-12 11:59:21 -0300

    Update lspsaga config

--- lua/user/plugins/lspsaga.lua
+++ lua/user/plugins/lspsaga.lua
@@ -26,7 +26,7 @@ map("n", "gr", "<cmd>Lspsaga rename<CR>", opts)
 map("n", "gd", "<cmd>Lspsaga preview_definition<CR>", opts)
 -- Show line and cursor diagnostics
 map("n", "gl", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
-map("n", "gl", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts)
+map("n", "gL", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts)
 -- Diagnostic jump
 map("n", "[e", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
 map("n", "]e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
