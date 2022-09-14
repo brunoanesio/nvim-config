857ea1b55cdbb7608f0d2a5679b839cb519c9dbe
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2022-09-14 12:03:45 -0300

    Basic winbar with navic

---
+++ lua/user/plugins/winbar.lua
@@ -0,0 +1,33 @@
+local M = {}
+
+M.winbar_filetype_exclude = {
+	"help",
+	"startify",
+	"dashboard",
+	"packer",
+	"neogitstatus",
+	"NvimTree",
+	"Trouble",
+	"alpha",
+	"lir",
+	"Outline",
+	"spectre_panel",
+	"toggleterm",
+	"DressingSelect",
+	"Jaq",
+	"harpoon",
+	"dapui_scopes",
+	"dapui_breakpoints",
+	"dapui_stacks",
+	"dapui_watches",
+	"dap-repl",
+	"dap-terminal",
+	"dapui_console",
+	"lab",
+	"Markdown",
+	"",
+}
+
+vim.o.winbar = "%{%v:lua.require'nvim-navic'.get_location()%}"
+
+return M
