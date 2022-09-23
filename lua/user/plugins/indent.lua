713bc06b36903249168f518e7e1ebb676806f0db
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2022-09-23 02:53:32 -0300

    Update indent config

--- lua/user/plugins/indent.lua
+++ lua/user/plugins/indent.lua
@@ -11,4 +11,12 @@ require("indent_blankline").setup({
 	show_end_of_line = false,
 	show_current_context = true,
 	show_current_context_start = true,
+	char_highlight_list = {
+		"IndentBlanklineIndent1",
+		"IndentBlanklineIndent2",
+		"IndentBlanklineIndent3",
+		"IndentBlanklineIndent4",
+		"IndentBlanklineIndent5",
+		"IndentBlanklineIndent6",
+	},
 })
