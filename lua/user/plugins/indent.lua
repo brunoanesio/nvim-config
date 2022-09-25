66e39e916be872d05fa74a7c4600a66a8b88f6c4
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2022-09-25 17:25:00 -0300

    Revert colored indent

--- lua/user/plugins/indent.lua
+++ lua/user/plugins/indent.lua
@@ -11,12 +11,12 @@ require("indent_blankline").setup({
 	show_end_of_line = false,
 	show_current_context = true,
 	show_current_context_start = true,
-	char_highlight_list = {
-		"IndentBlanklineIndent1",
-		"IndentBlanklineIndent2",
-		"IndentBlanklineIndent3",
-		"IndentBlanklineIndent4",
-		"IndentBlanklineIndent5",
-		"IndentBlanklineIndent6",
-	},
+	-- char_highlight_list = {
+	-- 	"IndentBlanklineIndent1",
+	-- 	"IndentBlanklineIndent2",
+	-- 	"IndentBlanklineIndent3",
+	-- 	"IndentBlanklineIndent4",
+	-- 	"IndentBlanklineIndent5",
+	-- 	"IndentBlanklineIndent6",
+	-- },
 })
