8ef4b1f974b46f2ad05a66a2205bc5972c80f712
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2022-09-26 22:14:16 -0300

    Disable unused plugins, remove bufferline

--- lua/user/plugins/bufferline.lua
+++
@@ -1,9 +0,0 @@
-require("bufferline").setup({
-	options = {
-		diagnostics = "nvim_lsp",
-		diagnostics_update_insert = true,
-		offsets = { { filetype = "NvimTree", text = "File Explorer" } },
-		always_show_bufferline = false,
-		show_close_icon = false,
-	},
-})
