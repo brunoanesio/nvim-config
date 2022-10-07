9b16160c4a729d2f354c6437e3cbcf9729cf2620
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2022-10-07 07:42:40 -0300

    Change feline icons

--- lua/user/plugins/feline.lua
+++ lua/user/plugins/feline.lua
@@ -1,6 +1,28 @@
 local ctp_feline = require("catppuccin.groups.integrations.feline")
 
--- ctp_feline.setup()
+ctp_feline.setup({
+	assets = {
+		left_separator = "",
+		right_separator = "",
+		bar = "█",
+		mode_icon = "",
+		dir = "  ",
+		file = "   ",
+		lsp = {
+			server = "  ",
+			error = "  ",
+			warning = "  ",
+			info = "  ",
+			hint = "  ",
+		},
+		git = {
+			branch = "  ",
+			added = "  ",
+			changed = "  ",
+			removed = "  ",
+		},
+	},
+})
 
 require("feline").setup({
 	components = ctp_feline.get(),
