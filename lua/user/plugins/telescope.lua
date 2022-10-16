da1828638b0f8763b9d050202096023955676fdc
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2022-10-16 01:44:40 -0300

    Fix telescope mapping not working

--- lua/user/plugins/telescope.lua
+++ lua/user/plugins/telescope.lua
@@ -6,72 +6,76 @@ require("telescope").setup({
 		path_display = { "smart" },
 		layout_strategy = "horizontal",
 		layout_config = {
-			prompt_position = "top",
-			width = 0.9,
+			prompt_position = "bottom",
+			width = 0.8,
 			preview_width = 0.6,
-			preview_cutoff = 80,
+			preview_cutoff = 40,
 		},
-	},
-	mappings = {
-		i = {
-			["<C-n>"] = actions.cycle_history_next,
-			["<C-p>"] = actions.cycle_history_prev,
-
-			["<C-j>"] = actions.move_selection_next,
-			["<C-k>"] = actions.move_selection_previous,
-
-			["<C-b>"] = actions.results_scrolling_up,
-			["<C-f>"] = actions.results_scrolling_down,
-
-			["<C-c>"] = actions.close,
-
-			["<Down>"] = actions.move_selection_next,
-			["<Up>"] = actions.move_selection_previous,
-
-			["<CR>"] = actions.select_default,
-			["<C-s>"] = actions.select_horizontal,
-			["<C-v>"] = actions.select_vertical,
-			["<C-t>"] = actions.select_tab,
-			["<C-d>"] = actions.delete_buffer,
-
-			["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-			["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-			["<C-l>"] = actions.complete_tag,
-			["<C-h>"] = actions.which_key,
-			["<esc>"] = actions.close,
+		preview = {
+			hide_on_startup = true,
 		},
-		n = {
-			["<esc>"] = actions.close,
-			["<CR>"] = actions.select_default,
-			["<C-x>"] = actions.select_horizontal,
-			["<C-v>"] = actions.select_vertical,
-			["<C-t>"] = actions.select_tab,
-			["<C-b>"] = actions.results_scrolling_up,
-			["<C-f>"] = actions.results_scrolling_down,
+		mappings = {
+			i = {
+				["<C-j>"] = actions.cycle_history_next,
+				["<C-k>"] = actions.cycle_history_prev,
 
-			["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-			["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+				["<C-n>"] = actions.move_selection_next,
+				["<C-p>"] = actions.move_selection_previous,
 
-			["j"] = actions.move_selection_next,
-			["k"] = actions.move_selection_previous,
-			["H"] = actions.move_to_top,
-			["M"] = actions.move_to_middle,
-			["L"] = actions.move_to_bottom,
-			["q"] = actions.close,
-			["dd"] = require("telescope.actions").delete_buffer,
-			["s"] = actions.select_horizontal,
-			["v"] = actions.select_vertical,
-			["t"] = actions.select_tab,
+				["<C-b>"] = actions.results_scrolling_up,
+				["<C-f>"] = actions.results_scrolling_down,
 
-			["<Down>"] = actions.move_selection_next,
-			["<Up>"] = actions.move_selection_previous,
-			["gg"] = actions.move_to_top,
-			["G"] = actions.move_to_bottom,
+				["<C-c>"] = actions.close,
 
-			["<PageUp>"] = actions.results_scrolling_up,
-			["<PageDown>"] = actions.results_scrolling_down,
+				["<Down>"] = actions.move_selection_next,
+				["<Up>"] = actions.move_selection_previous,
 
-			["?"] = actions.which_key,
+				["<CR>"] = actions.select_default,
+				["<C-s>"] = actions.select_horizontal,
+				["<C-v>"] = actions.select_vertical,
+				["<C-t>"] = actions.select_tab,
+				["<C-d>"] = require("telescope.actions").delete_buffer,
+
+				["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+				["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+				["<C-l>"] = actions.complete_tag,
+				["<C-h>"] = actions.which_key,
+				["<esc>"] = actions.close,
+				["<C-x>"] = require("telescope.actions.layout").toggle_preview,
+			},
+			n = {
+				["<CR>"] = actions.select_default,
+				["<esc>"] = actions.close,
+				["<C-x>"] = actions.select_horizontal,
+				["<C-v>"] = actions.select_vertical,
+				["<C-t>"] = actions.select_tab,
+				["<C-b>"] = actions.results_scrolling_up,
+				["<C-f>"] = actions.results_scrolling_down,
+
+				["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+				["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+
+				["j"] = actions.move_selection_next,
+				["k"] = actions.move_selection_previous,
+				["H"] = actions.move_to_top,
+				["M"] = actions.move_to_middle,
+				["L"] = actions.move_to_bottom,
+				["q"] = actions.close,
+				["dd"] = require("telescope.actions").delete_buffer,
+				["s"] = actions.select_horizontal,
+				["v"] = actions.select_vertical,
+				["t"] = actions.select_tab,
+
+				["<Down>"] = actions.move_selection_next,
+				["<Up>"] = actions.move_selection_previous,
+				["gg"] = actions.move_to_top,
+				["G"] = actions.move_to_bottom,
+
+				["<PageUp>"] = actions.results_scrolling_up,
+				["<PageDown>"] = actions.results_scrolling_down,
+
+				["?"] = actions.which_key,
+			},
 		},
 	},
 	extensions = {
