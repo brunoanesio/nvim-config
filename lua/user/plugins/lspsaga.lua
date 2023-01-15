ccd1a639887197f9562400c382ce8f1a5472577d
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2023-01-15 18:30:37 -0300

    Update lspsaga config

--- lua/user/plugins/lspsaga.lua
+++ lua/user/plugins/lspsaga.lua
@@ -1,46 +1,33 @@
 local map = vim.keymap.set
-local opts = { silent = true }
-local saga = require("lspsaga")
 
 -- Config
-saga.init_lsp_saga({
-	move_in_saga = { prev = "<C-p>", next = "<C-n>" },
-	max_preview_lines = 15,
-	code_action_icon = "",
-	border_style = "rounded",
-	diagnostic_header = { " ", " ", " ", "" },
-	code_action_lightbulb = {
-		enable = true,
-		enable_in_insert = false,
-		sign = true,
-	},
+require("lspsaga").setup({
 	symbol_in_winbar = {
 		enable = false,
-		separator = "  ",
-		show_file = true,
-		in_custom = false,
 	},
-	show_outline = {
-		win_width = 35,
+	ui = {
+		border = "rounded",
+		code_action = "",
+		kind = require("catppuccin.groups.integrations.lsp_saga").custom_kind(),
+		colors = require("catppuccin.groups.integrations.lsp_saga").custom_colors(),
 	},
-	custom_kind = require("catppuccin.groups.integrations.lsp_saga").custom_kind(),
 })
 
 -- Mappings
 -- Lsp finder
-map("n", "gh", "<cmd>Lspsaga lsp_finder<CR>", opts)
+map("n", "gh", "<cmd>Lspsaga lsp_finder<CR>")
 -- Code action
-map({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts)
+map({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>")
 -- Rename
-map("n", "gr", "<cmd>Lspsaga rename<CR>", opts)
+map("n", "gr", "<cmd>Lspsaga rename<CR>")
 -- Definition preview
-map("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts)
+map("n", "gd", "<cmd>Lspsaga peek_definition<CR>")
 -- Show line and cursor diagnostics
-map("n", "gl", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
-map("n", "gL", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts)
+map("n", "gl", "<cmd>Lspsaga show_line_diagnostics<CR>")
+map("n", "gL", "<cmd>Lspsaga show_cursor_diagnostics<CR>")
 -- Diagnostic jump
-map("n", "[e", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
-map("n", "]e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
+map("n", "[e", "<cmd>Lspsaga diagnostic_jump_next<CR>")
+map("n", "]e", "<cmd>Lspsaga diagnostic_jump_prev<CR>")
 map("n", "[E", function()
 	require("lspsaga.diagnostic").goto_prev({ severity = vim.diagnostic.severity.ERROR })
 end, { silent = true })
@@ -48,6 +35,6 @@ map("n", "]E", function()
 	require("lspsaga.diagnostic").goto_next({ severity = vim.diagnostic.severity.ERROR })
 end, { silent = true })
 -- Outline
-map("n", "<leader>ol", "<cmd>LSoutlineToggle<CR>", opts)
+map("n", "<leader>ol", "<cmd>LSoutlineToggle<CR>")
 -- Hover
-map("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
+map("n", "K", "<cmd>Lspsaga hover_doc<CR>")
