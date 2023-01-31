dd23176cef22800f15d31b1300c19f145a17a050
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2023-01-31 03:52:59 -0300

    Update plugins config

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -3,40 +3,26 @@ return {
 		"numToStr/Comment.nvim",
 		opts = {
 			---Add a space b/w comment and the line
-			---@type boolean|fun():boolean
 			padding = true,
-
 			---Whether the cursor should stay at its position
-			---NOTE: This only affects NORMAL mode mappings and doesn't work with dot-repeat
-			---@type boolean
 			sticky = true,
-
-			---Lines to be ignored while comment/uncomment.
-			---Could be a regex string or a function that returns a regex string.
-			---Example: Use '^$' to ignore empty lines
-			---@type string|fun():string
+			---Lines to be ignored while (un)comment
 			ignore = nil,
-
-			---LHS of toggle mappings in NORMAL + VISUAL mode
-			---@type table
+			---LHS of toggle mappings in NORMAL mode
 			toggler = {
 				---Line-comment toggle keymap
 				line = "gcc",
 				---Block-comment toggle keymap
 				block = "gbc",
 			},
-
-			---LHS of operator-pending mappings in NORMAL + VISUAL mode
-			---@type table
+			---LHS of operator-pending mappings in NORMAL and VISUAL mode
 			opleader = {
 				---Line-comment keymap
 				line = "gc",
 				---Block-comment keymap
 				block = "gb",
 			},
-
 			---LHS of extra mappings
-			---@type table
 			extra = {
 				---Add comment on the line above
 				above = "gcO",
@@ -45,29 +31,17 @@ return {
 				---Add comment at the end of line
 				eol = "gcA",
 			},
-
-			---Create basic (operator-pending) and extended mappings for NORMAL + VISUAL mode
-			---NOTE: If `mappings = false` then the plugin won't create any mappings
-			---@type boolean|table
+			---Enable keybindings
+			---NOTE: If given `false` then the plugin won't create any mappings
 			mappings = {
-				---Operator-pending mapping
-				---Includes `gcc`, `gbc`, `gc[count]{motion}` and `gb[count]{motion}`
-				---NOTE: These mappings can be changed individually by `opleader` and `toggler` config
+				---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
 				basic = true,
-				---Extra mapping
-				---Includes `gco`, `gcO`, `gcA`
+				---Extra mapping; `gco`, `gcO`, `gcA`
 				extra = true,
-				---Extended mapping
-				---Includes `g>`, `g<`, `g>[count]{motion}` and `g<[count]{motion}`
-				extended = false,
 			},
-
-			---Pre-hook, called before commenting the line
-			---@type fun(ctx: Ctx):string
+			---Function to call before (un)comment
 			pre_hook = nil,
-
-			---Post-hook, called after commenting is done
-			---@type fun(ctx: Ctx)
+			---Function to call after (un)comment
 			post_hook = nil,
 		},
 		config = function(_, opts)
