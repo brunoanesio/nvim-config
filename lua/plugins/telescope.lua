8d113e2ebd12495b9d706c81d75602d9d6d18682
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2023-03-02 06:47:42 -0300

    Load notify extension

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -119,6 +119,7 @@ return {
 			})
 			require("telescope").load_extension("fzf")
 			require("telescope").load_extension("file_browser")
+			require("telescope").load_extension("notify")
 		end,
 	},
 }
