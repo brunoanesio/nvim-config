05cef2faca8b05ee99aa3926e253ef7b492ae7c0
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2023-05-28 08:37:39 -0300

    updates

--- lua/plugins/modules/colorscheme.lua
+++ lua/plugins/modules/colorscheme.lua
@@ -4,86 +4,11 @@ return {
     name = "catppuccin",
     priority = 100,
     lazy = false,
-    config = function()
-      require("catppuccin").setup({
-        flavour = "mocha",
-        term_colors = false,
-        transparent_background = false,
-        no_italic = true,
-        show_end_of_buffer = false,
-        styles = {
-          comments = {},
-          conditionals = {},
-          loops = {},
-          functions = {},
-          keywords = {},
-          strings = {},
-          variables = {},
-          numbers = {},
-          booleans = {},
-          properties = {},
-          types = {},
-          operators = {},
-        },
-        integrations = {
-          barbecue = {
-            dim_dirname = true,
-            bold_basename = true,
-            dim_context = false,
-          },
-          cmp = true,
-          dashboard = true,
-          gitsigns = true,
-          illuminate = true,
-          lsp_saga = true,
-          mason = true,
-          markdown = true,
-          mini = true,
-          native_lsp = {
-            enabled = true,
-            virtual_text = {
-              errors = { "italic" },
-              hints = { "italic" },
-              warnings = { "italic" },
-              information = { "italic" },
-            },
-            underlines = {
-              errors = { "underline" },
-              hints = { "underline" },
-              warnings = { "underline" },
-              information = { "underline" },
-            },
-          },
-          noice = true,
-          notify = true,
-          neotree = true,
-          telescope = true,
-          treesitter = true,
-          ts_rainbow2 = true,
-          which_key = true,
-        },
-        custom_highlights = function(colors)
-          return {
-            TelescopeSelection = { bg = colors.surface0 },
-            TelescopePromptCounter = { fg = colors.mauve },
-            TelescopePromptPrefix = { bg = colors.surface0 },
-            TelescopePromptNormal = { bg = colors.surface0 },
-            TelescopeResultsNormal = { bg = colors.mantle },
-            TelescopePreviewNormal = { bg = colors.crust },
-            TelescopePromptBorder = { bg = colors.surface0, fg = colors.surface0 },
-            TelescopeResultsBorder = { bg = colors.mantle, fg = colors.mantle },
-            TelescopePreviewBorder = { bg = colors.crust, fg = colors.crust },
-            TelescopePromptTitle = { fg = colors.surface0, bg = colors.surface0 },
-            TelescopeResultsTitle = { fg = colors.mantle, bg = colors.mantle },
-            TelescopePreviewTitle = { fg = colors.crust, bg = colors.crust },
-            NeoTreeWinSeparator = { fg = colors.base, bg = colors.base },
-            DiagnosticVirtualTextError = { fg = colors.red, bg = colors.none },
-            DiagnosticVirtualTextWarn = { fg = colors.yellow, bg = colors.none },
-            DiagnosticVirtualTextInfo = { fg = colors.sky, bg = colors.none },
-            DiagnosticVirtualTextHint = { fg = colors.teal, bg = colors.none },
-          }
-        end,
-      })
+    opts = function()
+      return require("plugins.configs.catppuccin")
+    end,
+    config = function(_, opts)
+      require("catppuccin").setup(opts)
       vim.api.nvim_command("colorscheme catppuccin")
     end,
   },
