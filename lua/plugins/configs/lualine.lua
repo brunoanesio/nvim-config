629b650ed669e99fc2cf40597361dda751c263a8
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2023-06-17 04:03:35 -0300

    lualine theming

--- lua/plugins/configs/lualine.lua
+++ lua/plugins/configs/lualine.lua
@@ -1,5 +1,5 @@
-local sep_right = { right = "" }
-local sep_left = { left = "" }
+local sep_right = { right = "" }
+local sep_left = { left = "" }
 local options = {
   options = {
     icons_enabled = true,
@@ -15,19 +15,17 @@ local options = {
     lualine_a = { { "fancy_mode", separator = sep_right } },
     lualine_b = {
       { "branch", separator = sep_right },
-      { "diff", padding = { left = 0, right = 0 }, separator = sep_right } },
+      { "diff", padding = { left = 0, right = 1 }, separator = sep_right } },
     lualine_c = {
       { "filetype", icon_only = true, padding = { left = 1, right = 0 } },
       { "filename", path = 0 },
     },
     lualine_x = {
-      { "diagnostics", sources = { "nvim_lsp" }, symbols = { error = " ", warn = "! ", info = " ", hint = " " } },
-      "fancy_lsp_servers",
+      { "diagnostics", sources = { "nvim_lsp" }, symbols = { error = " ", warn = "! ", info = " ", hint = "󰛩 " } },
+      { "fancy_lsp_servers", icon = " " },
     },
-    lualine_y = {},
-    lualine_z = {
-      { "location", icon = { "", align = "left" }, separator = sep_left, padding = 0 },
-      { "progress", icon = { "", align = "left" } } },
+    lualine_y = { { "progress", icon = { "", align = "left" }, separator = sep_left } },
+    lualine_z = { { "fancy_location", icon = "", separator = sep_left, padding = { right = 1 } } },
   },
   extensions = { "neo-tree", "lazy" },
 }
