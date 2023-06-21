f4decc4ab741d4cdc9243189f49b415b3022d396
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2023-06-21 14:25:04 -0300

    updates

--- lua/plugins/configs/nulls.lua
+++ lua/plugins/configs/nulls.lua
@@ -27,7 +27,7 @@ local diagnostics = nls.builtins.diagnostics
 local code_actions = nls.builtins.code_actions
 
 M.sources = {
-  formatting.isort.with({ extra_args = { "--profile black" } }),
+  -- formatting.isort.with({ extra_args = { "--profile black" } }),
   formatting.black.with({ extra_args = { "--fast" } }),
   -- diagnostics.ruff,
   diagnostics.djlint,
@@ -36,8 +36,8 @@ M.sources = {
   formatting.prettier,
   formatting.shfmt,
   formatting.clang_format,
+  formatting.taplo,
   code_actions.shellcheck,
-  -- code_actions.eslint_d,
 }
 
 return M
