568716a7545fb09b3def380e5aee736127d722a2
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2023-07-16 10:45:20 -0300

    remove unnecessary stuff

--- lua/plugins/configs/formatter.lua
+++ lua/plugins/configs/formatter.lua
@@ -12,24 +12,6 @@ local opts = {
   filetype = {
     lua = {
       require("formatter.filetypes.lua").stylua,
-
-      function()
-        if util.get_current_buffer_file_name() == "special.lua" then
-          return nil
-        end
-
-        return {
-          exe = "stylua",
-          args = {
-            "--search-parent-directories",
-            "--stdin-filepath",
-            util.escape_path(util.get_current_buffer_file_path()),
-            "--",
-            "-",
-          },
-          stdin = true,
-        }
-      end,
     },
 
     python = {
