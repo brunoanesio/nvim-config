55fb065822d07e955e426bb6a7d39e8c45d5816c
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2023-11-20 14:35:06 -0300

    updates

--- lua/plugins/modules/tools.lua
+++ lua/plugins/modules/tools.lua
@@ -84,24 +84,4 @@ return {
       })
     end,
   },
-
-  {
-    "GCBallesteros/NotebookNavigator.nvim",
-    ft = { "ipynb", "python" },
-    dependencies = {
-      "hkupty/iron.nvim",
-      "anuvyklack/hydra.nvim",
-    },
-    config = function()
-      local nn = require("notebook-navigator")
-      nn.setup({ activate_hydra_keys = "<leader>h" })
-    end,
-    -- stylua: ignore
-    keys = {
-      { "]h", function() require("notebook-navigator").move_cell "d" end },
-      { "[h", function() require("notebook-navigator").move_cell "u" end },
-      { "<leader>X", "<cmd>lua require('notebook-navigator').run_cell()<cr>" },
-      { "<leader>x", "<cmd>lua require('notebook-navigator').run_and_move()<cr>" },
-    },
-  },
 }
